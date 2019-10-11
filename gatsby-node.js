7d80eb35054ae4aec6df6794a3afca62fba4b86c
Bailey Swartz <bailey@cactusauto.com>
2019-10-11 11:44:24 +0000

    Demonstrate error with /. This creates an incorrectly ordered match-paths.json, but for some reason gatsby serve always shows the root-glob page, even in app.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,9 @@
 exports.onCreatePage = async({page,actions})=>{
   const {createPage}=actions;
-  /*if (page.path.match(/^\/root-glob/)) {
+  if (page.path.match(/^\/root-glob/)) {
     page.matchPath = "/*";
     createPage(page);
-  }*/
+  }
   
   if (page.path.match(/^\/zapp-glob/)) {
     page.matchPath = "/app/*";
