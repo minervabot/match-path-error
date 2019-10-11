3f2f37ce35a281ed17695993e6455a806c37a735
Bailey Swartz <bailey@cactusauto.com>
2019-10-11 11:42:53 +0000

    Demonstrate error with /app

---
+++ gatsby-node.js
@@ -0,0 +1,12 @@
+exports.onCreatePage = async({page,actions})=>{
+  const {createPage}=actions;
+  /*if (page.path.match(/^\/root-glob/)) {
+    page.matchPath = "/*";
+    createPage(page);
+  }*/
+  
+  if (page.path.match(/^\/zapp-glob/)) {
+    page.matchPath = "/app/*";
+    createPage(page);
+  }
+}
---
+++ src/pages/zapp-glob.js
@@ -0,0 +1,3 @@
+import React from "react"
+
+export default () => <div>/app/* Page</div>
